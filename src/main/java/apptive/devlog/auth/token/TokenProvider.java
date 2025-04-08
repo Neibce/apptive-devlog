2b9fe00497e0f74d2c87d5c05cdcb291c8bee41a
Jun-Young Yang <y@jun0.dev>
2025-04-08 15:45:05 +0900

    Jwt용 TokenProvider 정의

---
+++ src/main/java/apptive/devlog/auth/token/TokenProvider.java
@@ -0,0 +1,92 @@
+package apptive.devlog.auth.token;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Component
+public class TokenProvider {
+    private final UserDetailsService userDetailsService;
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    public static final long ACCESS_TOKEN_TTL_MS = 1000L * 60 * 15; //15분
+    public static final long REFRESH_TOKEN_TTL = 365L; //365일
+
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateAccessToken(String userEmail) {
+        return createToken(userEmail, ACCESS_TOKEN_TTL_MS);
+    }
+
+    public String generateRefreshToken() {
+        return createUuid();
+    }
+
+    private String createUuid() {
+        return UUID.randomUUID().toString().replace("-", "");
+    }
+
+    private String createToken(String email, long validityMs) {
+        Date now = new Date();
+        Date expiry = new Date(now.getTime() + validityMs);
+        Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(now)
+                .expiration(expiry)
+                .signWith(key)
+                .compact();
+    }
+
+    public String getEmailFromAccessToken(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer "))
+            return bearerToken.substring(7);
+        return null;
+    }
+
+    public boolean validateToken(String token) {
+        Date expirationDate = Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getExpiration();
+        return !expirationDate.before(new Date());
+    }
+
+    public Authentication getAuthentication(String token) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(getEmailFromAccessToken(token));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+}
+
