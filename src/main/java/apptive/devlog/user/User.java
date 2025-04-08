21613e63856c6b510881766e5164a0efd34b7415
Jun-Young Yang <y@jun0.dev>
2025-04-08 15:40:03 +0900

    User entity 및 repository 정의

---
+++ src/main/java/apptive/devlog/user/User.java
@@ -0,0 +1,52 @@
+package apptive.devlog.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+    @Id
+    @Column(nullable = false, unique = true, updatable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    @Column(nullable = false)
+    private LocalDate birth;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Gender gender;
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    public enum Gender {
+        MALE, FEMALE, OTHER
+    }
+}
+
